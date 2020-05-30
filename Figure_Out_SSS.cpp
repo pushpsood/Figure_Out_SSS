dcdea38d5ea39d177ab3f5a770eec14879759eac
Pushp Sood <pushpsood27@gmail.com>
2020-05-30 09:59:34 +0530

    cpp

---
+++ Figure_Out_SSS.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>//for cin
+#include <cstdlib>
+using namespace std;
+
+typedef unsigned char byte;
+int main()
+{
+   byte p[32];
+   for(int i=0;i<32;i++)
+   {
+       cin>>p[i];
+   }
+
+}
